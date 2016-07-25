910205148ad8ec5cee0428f40d1746a9ea59e41b
suriyar1994 <surimail94@gmail.com>
2016-07-25 16:05:36 +0530

    Add files via upload

---
+++ ShoppingCartBackEnd/src/test/java/com/niit/shoppingcart/CategoryTest.java
@@ -0,0 +1,36 @@
+package com.niit.shoppingcart;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.niit.shoppingcart.dao.CategoryDAO;
+import com.niit.shoppingcart.model.Category;
+
+public class CategoryTest {
+	
+	
+	public static void main(String[] args) {
+		
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		
+		context.scan("com.niit.shoppingcart");
+		context.refresh();
+		
+		
+	   CategoryDAO categoryDAO = 	(CategoryDAO) context.getBean("categoryDAO");
+	   
+	   Category category = 	(Category) context.getBean("category");
+	   category.setId("CG120");
+	   category.setName("CGName120");
+	   category.setDescription("CGDesc120");
+	   
+	   
+	   categoryDAO.saveOrUpdate(category);
+	   
+	   
+	   
+	   
+		
+		
+	}
+
+}
